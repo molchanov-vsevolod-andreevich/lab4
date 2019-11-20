3d948e70c7dc1054c0490c6b9b7d1264cea915d6
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-21 01:41:47 +0300

    Scripted auto-commit on change (2019-11-21 01:41:47) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -10,13 +10,13 @@ import akka.stream.javadsl.Flow;
 
 import java.util.concurrent.CompletionStage;
 
-public class AkkaApp {
+public class MainHttp {
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create("routes");
 
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        
+
         MainHttp instance = new MainHttp(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 instance.createRoute(system).flow(system, materializer);
