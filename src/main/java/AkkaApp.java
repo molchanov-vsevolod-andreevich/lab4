adbf5bd995f33ad568ee015dbef6a7901235678f
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-20 23:01:05 +0300

    Scripted auto-commit on change (2019-11-20 23:01:05) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -15,7 +15,7 @@ public class AkkaApp {
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-//        MainHttp instance = new MainHttp(system);
+        MainHttp instance = new MainHttp(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 instance.createRoute(system).flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
