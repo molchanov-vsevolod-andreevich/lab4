fdd50420aaf246c9b2158a87f310fe8e680b1c19
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-21 02:51:01 +0300

    Scripted auto-commit on change (2019-11-21 02:51:01) by gitwatch.sh

--- src/main/java/StoreActor.java
+++ src/main/java/StoreActor.java
@@ -17,7 +17,7 @@ public class StoreActor extends AbstractActor {
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, m -> {
                     store.put(m.getKey(), m.getValue());
-                    System.out.println("receive message! "+m.toString());
+                    System.out.println("receive message! " + m.toString());
                 })
                 .match(GetMessage.class, req -> sender().tell(
                         new StoreMessage(req.getKey(), store.get(req.getKey())), self())
