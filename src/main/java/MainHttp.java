8d1e562a2cfcdc7888b2d04aae0929c2885c326d
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-21 02:10:42 +0300

    Scripted auto-commit on change (2019-11-21 02:10:42) by gitwatch.sh

--- src/main/java/MainHttp.java
+++ src/main/java/MainHttp.java
@@ -40,6 +40,28 @@ public class MainHttp extends AllDirectives {
     }
 
     private Route createRoute() {
-        
+        return route(
+                path("semaphore", () ->
+                        route(
+                                get( () -> {
+                                    Future<Object> result = Patterns.ask(testPackageActor,
+                                            SemaphoreActor.makeRequest(), 5000);
+                                    return completeOKWithFuture(result, Jackson.marshaller());
+                                }))),
+                path("test", () ->
+                        route(
+                                post(() ->
+                                        entity(Jackson.unmarshaller(TestPackageMsg.class), msg -> {
+                                            testPackageActor.tell(msg, ActorRef.noSender());
+                                            return complete("Test started!");
+                                        })))),
+                path("put", () ->
+                        get(() ->
+                                parameter("key", (key) ->
+                                        parameter("value", (value) ->
+                                        {
+                                            storeActor.tell(new StoreActor.StoreMessage(key, value), ActorRef.noSender());
+                                            return complete("value saved to store ! key=" + key + " value=" + value);
+                                        })))),
     }
 }
