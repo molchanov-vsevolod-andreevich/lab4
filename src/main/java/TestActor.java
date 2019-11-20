8d0afca6fb7af1e9a4187052ec72f212ebb55f4d
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-21 00:31:21 +0300

    Scripted auto-commit on change (2019-11-21 00:31:21) by gitwatch.sh

--- src/main/java/TestActor.java
+++ src/main/java/TestActor.java
@@ -1,9 +1,16 @@
 import akka.actor.AbstractActor;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class TestActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        return null;
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(jscript);
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(functionName, params).toString();
     }
 }
