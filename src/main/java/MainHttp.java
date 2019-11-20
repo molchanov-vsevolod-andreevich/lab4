1b73b882a6081f26f2fc1593b54b27f8e11c104a
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-21 02:36:14 +0300

    Scripted auto-commit on change (2019-11-21 02:36:14) by gitwatch.sh

--- src/main/java/MainHttp.java
+++ src/main/java/MainHttp.java
@@ -62,10 +62,7 @@ public class MainHttp extends AllDirectives {
                                         {
                                             storeActor.tell(new StoreActor.StoreMessage(key, value), ActorRef.noSender());
                                             return complete("value saved to store ! key=" + key + " value=" + value);
-                                        })))),
-                (value) -> {
-                        storeActor.tell(new StoreActor.StoreMessage(key, value), ActorRef.noSender());
-                        return complete("value saved to store ! key=" + key + " value=" + value);
+                                        })))));
         }
     }
 }
