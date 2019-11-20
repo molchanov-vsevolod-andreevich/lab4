294028d29b2c39c28851da676c0128823a7a84d9
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-21 00:28:30 +0300

    Scripted auto-commit on change (2019-11-21 00:28:30) by gitwatch.sh

--- src/main/java/TestActor.java
+++ src/main/java/TestActor.java
@@ -1,3 +1,9 @@
-public class TestActor {
+import akka.actor.AbstractActor;
 
+public class TestActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
