d4761bc5d275924dba561bb0be8efed7fbef845b
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-21 16:49:14 +0300

    Scripted auto-commit on change (2019-11-21 16:49:14) by gitwatch.sh

--- src/main/java/RouteActor.java
+++ src/main/java/RouteActor.java
@@ -8,11 +8,10 @@ public class RouteActor extends AbstractActor {
     private ActorRef storeActor;
     private ActorRef testPackageActor;
 
-    private MainHttp(final ActorSystem system) {
+    private RouteActor(final ActorSystem system) {
         storeActor = system.actorOf(StoreActor.props(), "storeActor");
         testPackageActor = system.actorOf(new RoundRobinPool(5).props(Props.create(TestPackageActor.class, storeActor)), "testActor");
     }
-
 
     @Override
     public Receive createReceive() {
