9e9e8e3fa2ac70652f965a525c4739b3d60b6315
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-22 19:34:50 +0300

    Scripted auto-commit on change (2019-11-22 19:34:50) by gitwatch.sh

--- src/main/java/TestPackageMsg.java
+++ src/main/java/TestPackageMsg.java
@@ -1,3 +1,19 @@
-public class TestPackageMsg {
+public class TestPackageMessage {
+    private final int id;
+    private final String name;
+
+    @JsonCreator
+    public Pet(@JsonProperty("id") int id, @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
