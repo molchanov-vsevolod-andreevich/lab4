5208e169523f8bf466f93dffd2ecac5f77a886e4
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-22 17:36:30 +0300

    Scripted auto-commit on change (2019-11-22 17:36:30) by gitwatch.sh

--- src/main/java/RouteActor.java
+++ src/main/java/RouteActor.java
@@ -1,10 +1,15 @@
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.routing.RoundRobinPool;
 
 public class RouteActor extends AbstractActor {
-
     private ActorRef storeActor;
     private ActorRef testPackageActor;
+
+    RouteActor() {
+        storeActor = system.actorOf(StoreActor.props(), "storeActor");
+        testPackageActor = system.actorOf(new RoundRobinPool(5).props(TestPackageActor.props()), "testActor");
+    }
 
     @Override
     public Receive createReceive() {
