11dfc43930b06737ec37b0f446ab9b38d9fe7494
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-23 00:30:24 +0300

    Scripted auto-commit on change (2019-11-23 00:30:24) by gitwatch.sh

--- src/main/java/TestPackageActor.java
+++ src/main/java/TestPackageActor.java
@@ -9,7 +9,7 @@ import javax.script.ScriptEngineManager;
 public class TestPackageActor extends AbstractActor {
 
     public static Props props() {
-        return Props.create(StoreActor.class);
+        return Props.create(TestPackageActor.class);
     }
 
     @Override
