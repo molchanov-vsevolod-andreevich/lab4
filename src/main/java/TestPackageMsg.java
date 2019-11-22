3a9a8f5c01cb9a812fc29f2caea637b3377cf609
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-22 19:44:38 +0300

    Scripted auto-commit on change (2019-11-22 19:44:38) by gitwatch.sh

--- src/main/java/TestPackageMsg.java
+++ src/main/java/TestPackageMsg.java
@@ -5,15 +5,17 @@ public class TestPackageMsg {
     private final String packageId;
     private final String jsScript;
     private final String functionName;
-    private final Test tests;
+    private final Test[] tests;
 
     @JsonCreator
     public TestPackageMsg(@JsonProperty("packageId") String packageId,
                           @JsonProperty("jsScript") String jsScript,
                           @JsonProperty("functionName") String functionName,
-                          @JsonProperty("tests")) {
-        this.packageId = ;
-        this.name = name;
+                          @JsonProperty("tests") Test[] tests) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
     }
 
     public int getId() {
