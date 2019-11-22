026f56782509922a6584723b7075f01fd526692c
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-22 23:53:20 +0300

    Scripted auto-commit on change (2019-11-22 23:53:20) by gitwatch.sh

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,2 +1,34 @@
-public class Test {
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonAutoDetect
+public static class Test {
+    @JsonProperty("testName")
+    private final String testName;
+    @JsonProperty("expectedResult")
+    private final String expectedResult;
+    @JsonProperty("params")
+    private final Object[] params;
+
+    public Test() {}
+
+    public Test(String testName,
+                String expectedResult,
+                Object[] params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
 }
