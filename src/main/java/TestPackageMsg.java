0613b1febdb7e4fab4cbfedb364fd2f9881beea0
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-22 19:49:28 +0300

    Scripted auto-commit on change (2019-11-22 19:49:28) by gitwatch.sh

--- src/main/java/TestPackageMsg.java
+++ src/main/java/TestPackageMsg.java
@@ -18,29 +18,46 @@ public class TestPackageMsg {
         this.tests = tests;
     }
 
-    public int getId() {
-        return id;
+    public String getPackageId() {
+        return packageId;
     }
 
-    public String getName() {
-        return name;
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public Test[] getTests() {
+        return tests;
     }
 
     public class Test {
         private final String testName;
         private final String expectedResul;
-        private final String functionName;
-        private final Test[] tests;
+        private final String params;
 
         @JsonCreator
-        public TestPackageMsg(@JsonProperty("packageId") String packageId,
-                              @JsonProperty("jsScript") String jsScript,
-                              @JsonProperty("functionName") String functionName,
-                              @JsonProperty("tests") Test[] tests) {
-            this.packageId = packageId;
-            this.jsScript = jsScript;
-            this.functionName = functionName;
-            this.tests = tests;
+        public Test(@JsonProperty("testName") String testName,
+                    @JsonProperty("expectedResul") String expectedResul,
+                    @JsonProperty("params") String params) {
+            this.testName = testName;
+            this.expectedResul = expectedResul;
+            this.params = params;
+        }
+
+        public String getTestName() {
+            return testName;
+        }
+
+        public String getExpectedResul() {
+            return expectedResul;
+        }
+
+        public String getParams() {
+            return params;
         }
     }
 }
