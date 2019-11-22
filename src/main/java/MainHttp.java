cd34a8bfe81bb79c61db5b1e61d88e12ff144b32
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-22 17:39:07 +0300

    Scripted auto-commit on change (2019-11-22 17:39:07) by gitwatch.sh

--- src/main/java/MainHttp.java
+++ src/main/java/MainHttp.java
@@ -34,7 +34,7 @@ public class MainHttp extends AllDirectives {
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
-                materializer
+                materialize
         );
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
