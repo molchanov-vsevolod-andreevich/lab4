b881048486dd322661b6cee245839974481a3acd
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-23 13:26:02 +0300

    Scripted auto-commit on change (2019-11-23 13:26:02) by gitwatch.sh

--- src/main/java/RouteActor.java
+++ src/main/java/RouteActor.java
@@ -6,11 +6,11 @@ import akka.routing.RoundRobinPool;
 
 public class RouteActor extends AbstractActor {
     private final ActorRef storeActor;
-    private final ActorRef testPackageActor;
+    private final ActorRef runTestActor;
 
     public RouteActor() {
         storeActor = getContext().actorOf(StoreActor.props(), AkkaAppConstants.STORE_ACTOR_NAME);
-        testPackageActor = getContext().actorOf(new RoundRobinPool(5).props(RunTestActor.props(storeActor)), AkkaAppConstants.RUN_TEST_ACTOR_NAME);
+        runTestActor = getContext().actorOf(new RoundRobinPool(5).props(RunTestActor.props(storeActor)), AkkaAppConstants.RUN_TEST_ACTOR_NAME);
     }
 
     static Props props() {
@@ -25,7 +25,7 @@ public class RouteActor extends AbstractActor {
                 })
                 .match(TestPackageRequest.class, msg -> {
                     for (TestPackageRequest.Test test : msg.getTests()) {
-                        testPackageActor.tell(new TestToEval(msg.getPackageId(),
+                        runTestActor.tell(new TestToEval(msg.getPackageId(),
                                         msg.getJsScript(),
                                         msg.getFunctionName(),
                                         test),
