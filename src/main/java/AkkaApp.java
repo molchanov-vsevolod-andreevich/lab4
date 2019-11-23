f13ca2410d42ceab76da32628d4b8277b4ed23b9
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-23 11:35:48 +0300

    Scripted auto-commit on change (2019-11-23 11:35:48) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -1,25 +1,19 @@
 import akka.NotUsed;
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
-import akka.http.javadsl.server.AllDirectives;
-import akka.http.javadsl.server.Route;
-import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 import java.util.concurrent.CompletionStage;
-import scala.concurrent.Future;
 
 public class AkkaApp {
 
     public static void main(String[] args) throws Exception {
-        ActorSystem system = ActorSystem.create("routes");
+        ActorSystem system = ActorSystem.create(AkkaAppConstants.ACTOR_SYSTEM_NAME);
 
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
@@ -29,10 +23,10 @@ public class AkkaApp {
                 instance.createRoute().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
-                ConnectHttp.toHost("localhost", 8080),
+                ConnectHttp.toHost(AkkaAppConstants.HOST, AkkaAppConstants.PORT),
                 materializer
         );
-        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.out.println(AkkaAppConstants.START_SERVER_MESSAGE);
         System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
--- src/main/java/AkkaAppConstants.java
+++ src/main/java/AkkaAppConstants.java
@@ -1,3 +1,10 @@
-public class AkkaAppConstants {
-    
+class AkkaAppConstants {
+    // Actors Names
+    static final String ACTOR_SYSTEM_NAME = "routes";
+
+    // Server Constants
+    static final String HOST = "localhost";
+    static final int PORT = 8080;
+    static final String START_SERVER_MESSAGE = "Server online at http://localhost:8080/\nPress RETURN to stop...";
+
 }
