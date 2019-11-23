6ae596e7e36bd8742cc5d496359f383fbdf5365a
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-23 13:05:28 +0300

    Scripted auto-commit on change (2019-11-23 13:05:28) by gitwatch.sh

--- src/main/java/StoreActor.java
+++ src/main/java/StoreActor.java
@@ -28,8 +28,7 @@ public class StoreActor extends AbstractActor {
                 })
                 .match(GetMessage.class, req -> {
                     TestPackageResponse.TestResult[] results new TestPackageResponse.TestResult[store.get(req.getKey()).size()];
-
-
+                    
                     sender().tell(
                             (store.get(req.getKey()) == null) ? AkkaAppConstants.NO_SUCH_PACKAGE_MESSAGE :
                                     new TestPackageResponse(req.getKey(),
