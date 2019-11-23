e3376c636c3cfaf473357ca76db4805fd4617255
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-11-23 11:10:50 +0300

    Scripted auto-commit on change (2019-11-23 11:10:50) by gitwatch.sh

--- src/main/java/HttpRouter.java
+++ src/main/java/HttpRouter.java
@@ -1,4 +1,11 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import akka.http.javadsl.server.AllDirectives;
 
 public class HttpRouter extends AllDirectives {
+    private final ActorRef routeActor;
+
+    private MainHttp(ActorSystem system) {
+        routeActor = system.actorOf(RouteActor.props(), "routeActor");
+    }
 }
